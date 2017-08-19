4b9f85ed8d6e751b76a935017aab215eee20f4f9
Rick <rickjiangshu@gmail.com>
2017-08-19 09:36:41 +0800

    add hittest

---
+++ HitTestUtil.cs
@@ -0,0 +1,32 @@
+﻿/*
+ * Author:  Rick
+ * Create:  2017/8/19 9:29:06
+ * Email:   rickjiangshu@gmail.com
+ * Follow:  https://github.com/RickJiangShu
+ */
+using UnityEngine;
+
+/// <summary>
+/// 用于碰撞检测
+/// </summary>
+public class HitTestUtil
+{
+    /// <summary>
+    /// 范围检测
+    /// </summary>
+    /// <returns></returns>
+    public static bool Range(Vector3 self, Vector3 target, float radius)
+    {
+        return Vector3.Distance(self, target) < radius;
+    }
+    /// <summary>
+    /// 范围检测（平面）
+    /// </summary>
+    /// <returns></returns>
+    public static bool RangePlane(Vector3 self, Vector3 target, float radius)
+    {
+        float dx = self.x - target.x;
+        float dy = self.y - target.y;
+        return Mathf.Sqrt(dx * dx + dy * dy) < radius;
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Utils
 仅依赖UnityEngine和C#标准库的静态方法集
+
+为何命名为XXUtil，而不是XXUtils？
+因为我认为一个工具类只做一件事。因此Util指一个工具类，而Utils指整个工具包。
